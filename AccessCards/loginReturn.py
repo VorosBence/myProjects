60c02f388af6aa36596a3718f2e176122f2b7cee
Voros Bence <93202648+VorosBence@users.noreply.github.com>
2023-03-18 16:52:27 +0100

    Add files via upload

--- AccessCards/loginReturn.py
+++ AccessCards/loginReturn.py
@@ -1,5 +1,32 @@
-
 class loginReturn():
     def __init__(self,db):
         self.db = db
-        print(db)
+
+    def argsCheck(self):
+        pid = False
+        byr = False
+        cid = False
+        iyr = False
+        eyr = False
+        hgt = False
+        hcl = False
+        ecl = False
+        for line in self.db:
+            for data in line:
+                if data == 'pid':
+                    pid = True
+                elif data == 'byr':
+                    byr = True
+                elif data == 'cid':
+                    cid = True
+                elif data == 'iyr':
+                    iyr = True
+                elif data == 'eyr':
+                    eyr = True
+                elif data == 'hgt':
+                    hgt = True
+                elif data == 'hcl':
+                    hcl = True
+                elif data == 'ecl':
+                    ecl = True
+
