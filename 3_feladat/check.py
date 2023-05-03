f0242d3a4550a1142a3f04a48dc739949aaea313
Voros Bence <93202648+VorosBence@users.noreply.github.com>
2023-05-03 10:29:25 +0200

    Add files via upload

--- 3_feladat/check.py
+++ 3_feladat/check.py
@@ -2,6 +2,7 @@ answers = []
 group = []
 allowed = []
 characters = 'abcdefghijklmnopqrstuvwxyz'
+ascii = [ord(char) for char in characters]
 with open('feladat3.txt','r') as file:
     for line in file:
         data = line.strip().split()
@@ -12,15 +13,34 @@ with open('feladat3.txt','r') as file:
         else:
             group.append(data)
 
-character2 = []
-for index, ans in enumerate(answers):
-    for indexy,data in enumerate(ans):
-        character2 = []
-        character2.append(d for d in data if not character2)
-        print(index,character2)
+
+
+def convert_character(__string__):
+    return list(set([ord(char) for char in __string__]))
+def check_character(__list__):
+    return len(characters) == len(__list__)
+
+    
     
 
 
 
 
+if check_character(convert_character(characters)):
+    allowed.append(1)
+else:
+    allowed.append(2)
 
+
+
+for index, ans in enumerate(answers):
+    temp = ''
+    for indexy,data in enumerate(ans):
+        temp += data
+    if check_character(convert_character(temp)):
+        allowed.append((index))
+
+
+print('Itt vannak azok a csoportok ahol az összes válasz szerepelt')
+for i in allowed:  
+    print(f'Index: {i}, {answers[i]}')
